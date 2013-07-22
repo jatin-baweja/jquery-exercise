8cde73a65211f785fcb0f8833289a71287e281dd
jatin-baweja <jatin.baweja@vinsol.com>
2013-07-22 19:51:06 +0530

    Updated Solution to jQuery Reveal Hidden Text Exercise

--- jquery-exercise6/js/blog.js
+++ jquery-exercise6/js/blog.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  $blogDiv = $('#blog');
+  var $blogDiv = $('#blog');
   //Add onclick listener and show excerpt paragraphs for clicked headings
   $blogDiv.delegate('h3', 'click', function(event) {
-    event.preventDefault();
     $blogDiv.find('h3').not($(this)).siblings('p').slideUp();
     $(this).siblings('p').slideDown();
+    event.preventDefault();
   });
 });
