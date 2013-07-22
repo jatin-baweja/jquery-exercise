553e7d02c8c7b66d63f1e311b6f6bde7cd5ee492
jatin-baweja <jatin.baweja@vinsol.com>
2013-07-22 19:30:38 +0530

    Updated Solution to jQuery Tabbed Navigation Exercise

--- jquery-exercise5/js/tabs.js
+++ jquery-exercise5/js/tabs.js
@@ -1,25 +1,22 @@
 $(document).ready(function() {
-  $modules = $('div.module');
+  var $modules = $('div.module');
   //Hide all modules
   $modules.hide();
   //Create unordered list
   var $unorderedList = $('<ul></ul>').insertBefore($modules.eq(0));
   //Iterate over the modules and add h2 text to li element. Also, link related module to li using data()
   $modules.each(function() {
-    $this = $(this);
-    $listElement = $('<li></li>').append($this.find('h2').text());
-    $listElement.data('relatedModule', $this);
-    $unorderedList.append($listElement);
+    var $this = $(this);
+    $unorderedList.append($('<li></li>').append($this.find('h2').text()).data('relatedModule', $this.attr('id')));
   });
   //Bind a click event to li element that shows related module and hides other modules
   $unorderedList.delegate('li', 'click', function() {
-    $this = $(this);
+    var $this = $(this);
     $modules.hide();
-    $this.data('relatedModule').show();
-    $this.siblings().removeClass('current');
-    $this.addClass('current');
+    $this.addClass('current').siblings().removeClass('current');
+    $('#' + $this.data('relatedModule')).show();
   });
   //Show the first tab
-  $unorderedList.children('li').first().data('relatedModule').show();
-  $unorderedList.children('li').first().addClass('current');
+  var firstTab = $unorderedList.children('li').first().addClass('current');
+  $('#' + firstTab.data('relatedModule')).show();
 });
