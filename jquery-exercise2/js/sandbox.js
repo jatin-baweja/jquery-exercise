5641eb99bec892a1d78d4b4ff70eb66e5256b73f
jatin-baweja <jatin.baweja@vinsol.com>
2013-07-22 18:01:27 +0530

    Updated Solution to jQuery Traversing Exercise

--- jquery-exercise2/js/sandbox.js
+++ jquery-exercise2/js/sandbox.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     console.log($(element).attr('alt'));
   });
   //Select search box, traverse upto form and add a class to the form
-  $('input[type="text"]').closest('form').addClass('search-form');
+  $('input[name="q"].input_text').closest('form').addClass('search-form');
   //Select list item with current class, remove that class from it and add it to the next list item
   $('#myList li').filter('.current').removeClass('current').next().addClass('current');
   //Select the select element inside #specials and traverse your way to the submit button
