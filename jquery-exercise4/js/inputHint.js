99c2b755f89f84e1a87b8bf24b5d6c4adc549446
jatin-baweja <jatin.baweja@vinsol.com>
2013-07-22 18:35:53 +0530

    Updated Solution to jQuery Input Hint Exercise

--- jquery-exercise4/js/inputHint.js
+++ jquery-exercise4/js/inputHint.js
@@ -1,26 +1,25 @@
 $(document).ready(function() {
   //Store Search input
   var $searchBox = $('#search .input_text');
+  var $searchBoxLabel = $searchBox.closest('form').find('label[for="' + $searchBox.attr("name") + '"]');
   //Store hint text for use later
-  $searchBox.data('hintText', $searchBox.closest('form').find('label[for="' + $searchBox.attr("name") + '"]').text());
-  $searchBox.val($searchBox.data('hintText'));
+  $searchBox.data('hintText', $searchBoxLabel);
+  $searchBox.val($searchBox.data('hintText').text());
   //Add hint class
   $searchBox.addClass('hint');
-  $searchBox.closest('form').find('label[for="' + $searchBox.attr("name") + '"]').remove();
+  $searchBoxLabel.remove();
   $searchBox.bind('focus', function() {
-    $this = $(this);
-    //Remove hint text only if search box class is hint and hint text string is present
-    if (typeof $this.val() == "string" && $this.val() == $this.data('hintText') && $this.hasClass('hint')) {
-      $this.val('');
-      $this.removeClass('hint');
+    var $this = $(this);
+    //Remove hint text only if search box class has hint and hint text string is present
+    if (typeof $this.val() == "string" && $this.val() == $this.data('hintText').text() && $this.hasClass('hint')) {
+      $this.val('').removeClass('hint');
     }
   });
   $searchBox.bind('blur', function() {
-    $this = $(this);
+    var $this = $(this);
     //If empty string is there, add hint text on blur and add class hint
     if (typeof $this.val() == "string" && $this.val() == "") {
-      $this.addClass('hint');
-      $this.val($this.data('hintText'));
+      $this.addClass('hint').val($this.data('hintText').text());
     }
   });
 });
