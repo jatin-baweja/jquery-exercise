ea75b96c4b9365a586b7e39add4c579ca91402d3
jatin-baweja <jatin.baweja@vinsol.com>
2013-07-25 15:11:48 +0530

    Updated Solution to Load content using JSON Exercise

--- jquery-exercise10/js/specials.js
+++ jquery-exercise10/js/specials.js
@@ -5,22 +5,25 @@ $(document).ready(function() {
   var $selectDropdown = $('#specials select[name="day"]');
   //Add one time event handler so that json is loaded only once
   $selectDropdown.one('change', function(event) {
-    var contentUrl = './data/specials.json';
+    var contentUrl = 'data/specials.json';
+    var oneContext = this;
     $.ajax({
       url : contentUrl,
       type : "GET",
       dataType : 'json',
+      //To ensure callback functions run in the one context
+      context : oneContext,
       success : function(json) {
         var recievedJSON = json;
         //Don't change target div if Select is selected
-        if (typeof event.target.value == "string" && event.target.value != "") {
-          var currentSpecial = recievedJSON[event.target.value];
+        if ($(this).val() != "") {
+          var currentSpecial = recievedJSON[$(this).val()];
           var htmlToShow = "<h3>" + currentSpecial.title + "</h3><img src ='." + currentSpecial.image + "'/><p style='color:" + currentSpecial.color + "'>" + currentSpecial.text + "</p>";
           $targetDiv.html(htmlToShow);
         }
         $selectDropdown.bind('change', function(event) {
-          if (typeof event.target.value == "string" && event.target.value != "") {
-            var currentSpecial = recievedJSON[event.target.value];
+          if ($(this).val() != "") {
+            var currentSpecial = recievedJSON[$(this).val()];
             var htmlToShow = "<h3>" + currentSpecial.title + "</h3><img src='." + currentSpecial.image + "'/><p style='color:" + currentSpecial.color + "'>" + currentSpecial.text + "</p>";
             $targetDiv.html(htmlToShow);
           }
