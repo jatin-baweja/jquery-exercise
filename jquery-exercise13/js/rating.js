58a8be3d727f7095b107a5d128406da5bf7fef89
jatin-baweja <jatin.baweja@vinsol.com>
2013-07-27 11:21:00 +0530

    Solution to jQuery Product Rating (Level 2) Exercise

---
+++ jquery-exercise13/js/rating.js
@@ -0,0 +1,40 @@
+$(function() {
+  //List of ratings
+  var ratings = ["Love it", "Like it", "No Views", "Dislike it", "Abhor it"];
+  var $ratingsContainer = $('#rating-values').before($('<div id="empty-div"></div>'));
+  //Create Rating Divs
+  $.each(ratings, function(index, value) {
+    $ratingsContainer.append($('<div class="rating" id="' + value.toLowerCase().replace(/ /g, '-') + '">' + value + '</div>'));
+  });
+  //Sample data for drinks
+  var drinks = ["Coffee", "Tea", "Sodas"];
+  //Function to create a new row for a product
+  var  newRow = function(productName) {
+    var $containerDiv = $('<div id="' + productName.toLowerCase().replace(/ /g, '-') + '-rating"></div>').appendTo($('#product-ratings'));
+    $containerDiv.append($('<div id="' + productName.toLowerCase().replace(/ /g, '-') + '" class="product">' + productName + '</div>'));
+    for (var i = 0, j = ratings.length; i < j; i++) {
+      $containerDiv.append($('<div class="radio-box"><input class="' + ratings[i].toLowerCase().replace(/ /g, '-') + '" type="radio" disabled /></div>'));
+    }
+  }
+  $.each(drinks, function(index, value) {
+    newRow(value);
+  });
+  //Selection of a row
+  $rowSelect = $('#product-ratings').delegate('.product', 'click', function() {
+    var $this = $(this);
+    //Toggle highlight class on clicks and remove highlight from other products
+    $this.toggleClass('highlight').parent().siblings().children('.product').removeClass('highlight');
+    //Remove highlight from ratings
+    $ratingsContainer.children('.highlight').removeClass('highlight');
+  });
+  var $products = $('.product');
+  $ratingsContainer.delegate('.rating', 'click', function() {
+    var $this = $(this);
+    //Toggle highlight class and remove highlight class from other ratings
+    $this.toggleClass('highlight').siblings().removeClass('highlight');
+    //If a product is selected, check its corresponding radio button and uncheck and disable all others for that product
+    if ($products.hasClass('highlight')) {
+      console.log($products.filter($('.highlight')).siblings().children('.' + $this.attr('id')).attr('disabled', false).prop('checked', true).parent().siblings().children('input[type="radio"]').attr('disabled', true).prop('checked', false));
+    }
+  });
+});
