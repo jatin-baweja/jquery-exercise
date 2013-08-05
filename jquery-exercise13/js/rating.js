baee6f7356a0480a7b4d4a76c9b370401d72e3f3
jatin-baweja <jatin.baweja@vinsol.com>
2013-08-05 18:06:38 +0530

    Added names to radio buttons. Removed console logs

--- jquery-exercise13/js/rating.js
+++ jquery-exercise13/js/rating.js
@@ -13,7 +13,7 @@ $(function() {
     var $containerDiv = $('<div id="' + productName.toLowerCase().replace(/ /g, '-') + '-rating"></div>').appendTo($('#product-ratings'));
     $containerDiv.append($('<div id="' + productName.toLowerCase().replace(/ /g, '-') + '" class="product">' + productName + '</div>'));
     for (var i = 0, j = ratings.length; i < j; i++) {
-      $containerDiv.append($('<div class="radio-box"><input class="' + ratings[i].toLowerCase().replace(/ /g, '-') + '" type="radio" disabled /></div>'));
+      $containerDiv.append($('<div class="radio-box"><input class="' + ratings[i].toLowerCase().replace(/ /g, '-') + '" type="radio" name="' + productName.toLowerCase().replace(/ /g, '-') + '-rating" disabled /></div>'));
     }
   }
   $.each(drinks, function(index, value) {
@@ -23,7 +23,8 @@ $(function() {
   $rowSelect = $('#product-ratings').delegate('.product', 'click', function() {
     var $this = $(this);
     //Toggle highlight class on clicks and remove highlight from other products
-    $this.toggleClass('highlight').parent().siblings().children('.product').removeClass('highlight');
+    $this.toggleClass('highlight');
+    $('.product').not($this).removeClass('highlight');
     //Remove highlight from ratings
     $ratingsContainer.children('.highlight').removeClass('highlight');
   });
@@ -32,9 +33,9 @@ $(function() {
     var $this = $(this);
     //Toggle highlight class and remove highlight class from other ratings
     $this.toggleClass('highlight').siblings().removeClass('highlight');
-    //If a product is selected, check its corresponding radio button and uncheck and disable all others for that product
+    //If a product is selected, check its corresponding radio button and disable all others for that product
     if ($products.hasClass('highlight')) {
-      console.log($products.filter($('.highlight')).siblings().children('.' + $this.attr('id')).attr('disabled', false).prop('checked', true).parent().siblings().children('input[type="radio"]').attr('disabled', true).prop('checked', false));
+      $('[name="' + $products.filter('.highlight').attr('id') + '-rating"]').attr('disabled', true).filter('.' + $this.attr('id')).attr('disabled', false).prop('checked', true);
     }
   });
 });
